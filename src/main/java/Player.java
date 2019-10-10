8b9072420bf05749d78ef7b3f95737a34c61baf4
Joe Cooper <josephcooper3@gmail.com>
2019-10-10 14:57:09 +0100

    adds hand and draw method to Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 public class Player {
 
     private String name;
+    private ArrayList<Card> hand;
 
     public Player(String name){
         this.name = name;
+        this.hand = new ArrayList<Card>();
     }
 
     public Object getName() {
         return this.name;
+    }
+
+    public int countHand() {
+        return this.hand.size();
+    }
+
+    public void drawCard(Deck deck) {
+        Card drawnCard = deck.dealCard();
+        this.hand.add(drawnCard);
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,15 +6,30 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
 
     Player player1;
+    Deck deck;
 
     @Before
     public void before(){
         player1 = new Player("Gert");
+        deck = new Deck();
+        deck.addAllCards();
+        deck.shuffle();
     }
 
     @Test
     public void canGetName(){
         assertEquals("Gert", player1.getName());
+    }
+
+    @Test
+    public void handStartsAtZero(){
+        assertEquals(0, player1.countHand());
+    }
+
+    @Test
+    public void canDrawCard(){
+        player1.drawCard(deck);
+        assertEquals(1, player1.countHand());
     }
 
 }

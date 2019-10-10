7ae7067846582a3c1c3c20061e426c0853f7e054
Joe Cooper <josephcooper3@gmail.com>
2019-10-10 11:53:23 +0100

    deck starts empty

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int getCardCount() {
+        return this.cards.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void deckStartsEmpty(){
+        assertEquals(0, deck.getCardCount());
+    }
+
 }
